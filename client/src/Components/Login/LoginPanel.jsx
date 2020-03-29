19dae91467496fe361ced1e2c47565b6f66a52f3
aishanya11 <aishanya11@gmail.com>
2020-03-30 00:35:25 +0530

    inrtegrated login

--- client/src/Components/Login/LoginPanel.jsx
+++ client/src/Components/Login/LoginPanel.jsx
@@ -5,13 +5,37 @@ import { Button, FormGroup, FormControl} from "react-bootstrap";
 export default function Login(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  var responsePost = {};
+
+
+
 
   function validateForm() {
     return username.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  async function handleSubmit (e) {
+    e.preventDefault();
+    console.log("hello its me")
+    const response = await fetch('/mentee/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({"username":username,"password":password}),
+    });
+    const body = await response.text();
+    responsePost = JSON.parse(body);
+    // console.log(body);
+    console.log(typeof(responsePost));
+    if(responsePost.success==true){
+      console.log(responsePost)
+      localStorage.setItem("user",JSON.stringify(responsePost.user));
+      localStorage.setItem("JWTtoken",responsePost.token)
+    }
+    else{
+      console.log("error");
+    }
   }
 
   return (
